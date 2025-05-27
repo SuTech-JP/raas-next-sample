b8067a7f5eed8cbdd7a4cdba60df21bc32743a43
鈴木順 <64670392+SuTechDevelop@users.noreply.github.com>
2025-05-27 17:00:36 +0900

    CORS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
+const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/api/:path*', // API ルートにマッチ
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: FRONTEND_ORIGIN || '' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
